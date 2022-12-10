8ccbc8a3a0480f94a1d6d0c3515fa4cf98315eec
efim1703 <17samara03@gmail.com>
2022-12-11 00:36:38 +0300

    Сделал шапку, подключил глобальные файлы scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <Tutorial/>
+  <div class="page">
+    <div class="title-block">
+      <div class="container">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,12 +13,20 @@ export default {
   name: 'IndexPage',
   data() {
     return {
-      list: [
-        {name: 'fsdfs', id: 1},
-        {name: 'fsdfs', id: 1},
-        {name: 'fsdfs', id: 1},
-      ]
+
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.page {
+  .title-block {
+    height: 848px;
+    background: linear-gradient(0deg, rgba(35, 38, 45, 0.9), rgba(35, 38, 45, 0.9)), url('@/assets/img/background.jpg');
+    backdrop-filter: blur(5px);
+
+
+  }
+}
+</style>
---
+++ pages/portfolio/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  Portfolio
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
