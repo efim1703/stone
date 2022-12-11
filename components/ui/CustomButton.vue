b822b74bf21b78f60ce72f21f82c368f6f7a3209
efim1703 <17samara03@gmail.com>
2022-12-11 20:10:48 +0300

    Добавил анимацию

--- components/ui/CustomButton.vue
+++ components/ui/CustomButton.vue
@@ -3,8 +3,8 @@
   <span class="title">
     {{ title }}
   </span>
-  <span class="icon">
-    <slot name="icon"/>
+  <span class="icon" v-if="arrow">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g><g><path d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068    c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557    l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104    c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z" fill="#ffffff" data-original="#000000" class=""/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></g></svg>
   </span>
 </button>
 </template>
@@ -17,6 +17,10 @@ export default {
     title: {
       type: String,
       default: ''
+    },
+    arrow: {
+      type: Boolean,
+      default: false
     }
   }
 }
@@ -36,11 +40,12 @@ export default {
     line-height: 2;
     text-transform: uppercase;
     letter-spacing: .05em;
-
   }
   .icon {
     width: fit-content;
     height: fit-content;
+    margin-left: 10px;
+
     svg {
       width: 15px;
       height: 15px;
--- pages/index.vue
+++ pages/index.vue
@@ -10,18 +10,22 @@
               на любой вкус
             </div>
             <div class="info-button">
-              <custom-button title="Расчитать стоимость">
-<!--                <template v-slot:icon>-->
-<!--                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g><g><path d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068    c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557    l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104    c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z" fill="#ffffff" data-original="#000000" class=""/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></g></svg>-->
-<!--                </template>-->
+              <custom-button title="Расчитать стоимость" arrow>
+                <template v-slot:icon>
+                </template>
               </custom-button>
             </div>
           </div>
         </div>
-        <div class="title-section-right">
-          qq2
+        <div class="title-section-right d-flex">
+          <img class="img first" src="@/assets/img/title-img1.png" >
+          <img class="img second" src="@/assets/img/title-img2.png" >
+          <img class="img third" src="@/assets/img/title-img3.png" >
         </div>
       </div>
+    </div>
+    <div class="section" style="height: 1000px">
+
     </div>
   </div>
 </template>
@@ -42,23 +46,89 @@ export default {
 
 <style lang="scss" scoped>
 .page {
+  overflow-x: hidden;
+
   .title-section {
     height: 764px;
     background: linear-gradient(0deg, rgba(35, 38, 45, 0.9), rgba(35, 38, 45, 0.9)), url('@/assets/img/background.jpg');
     backdrop-filter: blur(5px);
     padding-top: 84px;
+
     &-left {
       width: 40%;
       height: 100%;
       .info-text {
           font-weight: 700;
-          font-size: 40px
+          font-size: 40px;
+          margin-bottom: 28px;
         }
     }
     &-right {
       width: 60%;
       height: 100%;
+
+      .img {
+        background-size: cover;
+        &.first {
+          margin: auto auto -64px -160px;
+          width: calc(1038px * 0.8);
+          height: calc(950px * 0.8);
+          animation: first-img  2s;
+        }
+        &.second {
+          margin: auto auto 160px -328px;
+          width: calc(641px * 0.7);
+          height: calc(556px * 0.7);
+          animation: second-img 2.5s ;
+
+        }
+        &.third {
+          margin: auto auto 160px -168px;
+          width: calc(641px * 0.7);
+          height: calc(556px * 0.7);
+          animation: third-img 3s ;
+        }
+      }
     }
+  }
+}
+
+@keyframes first-img {
+  0% {
+    margin-left: 300px;
+    opacity: 0;
+  }
+  100% {
+    margin-left: -160px;
+    opacity: 1;
+  }
+}
+@keyframes second-img {
+  0% {
+    margin-left: -228px;
+    opacity: 0;
+  }
+  40% {
+    margin-left: -228px;
+    opacity: 0;
+  }
+  100% {
+    margin-left: -328px;
+    opacity: 1;
+  }
+}
+@keyframes third-img {
+  0% {
+    margin-left: -68px;
+    opacity: 0;
+  }
+  51.4% {
+    margin-left: -68px;
+    opacity: 0;
+  }
+  100% {
+    margin-left: -168px;
+    opacity: 1;
   }
 }
 </style>
