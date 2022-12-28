442e933f4728f69d4898fb0f4d0482ea9a305718
Ivan <pl_st@list.ru>
2022-12-29 01:34:28 +0300

    feat: added catalog title block

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -1,15 +1,64 @@
 <template>
-<div>
-  Catalog
-</div>
+  <div class="catalog">
+    <div class="container">
+      <div class="catalog-title">
+        <h3 class="h-subtitle">Каталог</h3>
+        <h1 class="h1-title">Каталог искусственного камня</h1>
+        <p>В нашем каталоге представлены более 1000 цветов искусственного камня для кухни, ванной и других
+          помещений вашего дома с различными эффектами, цветовыми вариантами и текстурами самых популярных
+          производителей и коллекций. Вся продукция находится в наличии на складе или будет доставлена в кратчашие
+          сроки.
+        </p>
+        <p>Вы можете заказать изделие из материала или приобрести листы искусственного камня.</p>
+      </div>
+      <div class="catalog-brands">FILLER</div>
+      <div class="catalog-main">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "index"
+  name: "index",
+  components: {}
+
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .catalog {
+    margin-top: 124px;
 
+    &-title {
+      width: 75%;
+
+      p {
+        margin-bottom: 28px;
+
+        @media (max-width: 768px) {
+          margin-bottom: 8px;
+        }
+      }
+
+      @media (max-width: 1024px) {
+        width: 100%;
+      }
+
+      @media (max-width: 768px) {
+        text-align: center;
+      }
+    }
+
+    &-brands {
+      background: red;
+      height: 102px;
+      width: 100%;
+      margin-bottom: 28px;
+
+      @media (max-width: 768px) {
+        margin-bottom: 12px;
+      }
+    }
+  }
 </style>
