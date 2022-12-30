360f491df2e1450b2ece3bd198bd6d8f0730df3a
efim <kolmakov@trivio.ru>
2022-12-30 23:59:19 +0300

    поменял инпут, добавил в калькулятор инпуты

--- components/TheCalculator.vue
+++ components/TheCalculator.vue
@@ -1,116 +1,156 @@
 <template>
- <div class="wrapper">
-   <div class="container">
-     <h3 class="h-subtitle">Калькултяор</h3>
-     <h2 class="h-title">Рассчитайте стоимость ваего изделия из искусственного камня</h2>
-     <div class="calculator">
-       <div class="header">
-         <div class="header-title">
-            ВЫБЕРИТЕ ФОРМУ СТОЛЕШНИЦЫ
-         </div>
-         <div class="header-radio d-flex align-center justify-between ">
-           <CustomButton
-               class="radio-btn"
-               title="ПРЯМАЯ"
-               text-center
-               :active="numberFormIsActive === 1"
-               @click="numberFormIsActive = 1"
-           />
-           <CustomButton
-               class="radio-btn"
-               title="Г-ОБРАЗНАЯ"
-               text-center
-               :active="numberFormIsActive === 2"
-               @click="numberFormIsActive = 2"
-           />
-           <CustomButton
-               class="radio-btn"
-               title="П-ОБРАЗНАЯ"
-               text-center
-               :active="numberFormIsActive === 3"
-               @click="numberFormIsActive = 3"
-           />
-         </div>
-       </div>
-       <div class="body d-flex ">
-         <div class="body-img">
-           <img :src="require(`@/assets/svg/calculator${ numberFormIsActive }.svg`)" width="100%">
-         </div>
-         <div class="body-text d-flex flex-column align-center">
-           <span>Укажите размеры (в мм)</span>
-           <div class="inputs">
-
-           </div>
-         </div>
-       </div>
-     </div>
-   </div>
- </div>
+    <div class="wrapper">
+        <div class="container">
+            <h3 class="h-subtitle">Калькултяор</h3>
+            <h2 class="h-title">Рассчитайте стоимость ваего изделия из искусственного камня</h2>
+            <div class="calculator">
+                <div class="header">
+                    <div class="header-title">
+                        ВЫБЕРИТЕ ФОРМУ СТОЛЕШНИЦЫ
+                    </div>
+                    <div class="header-radio d-flex align-center justify-between ">
+                        <CustomButton
+                            v-for="(name, index) of typeNames"
+                            :key="index"
+                            class="radio-btn"
+                            :title="name"
+                            text-center
+                            :active="numberFormIsActive === index + 1"
+                            @click="numberFormIsActive = index + 1"
+                        />
+                    </div>
+                </div>
+                <div class="body d-flex ">
+                    <div class="body-img">
+                        <img :src="require(`@/assets/svg/calculator${ numberFormIsActive }.svg`)" width="100%">
+                    </div>
+                    <div class="body-inputs d-flex flex-column align-center">
+                        <span>Укажите размеры (в мм)</span>
+                        <div class="input-list d-flex">
+                            <InputSize
+                                v-for="(item, index) of getActiveInputSizeList"
+                                :key="index"
+                                :title="item.title"
+                                @input="item.value = $event"
+                                :value="item.value"
+                                class="input-list-item"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import CustomButton from "~/components/ui/CustomButton.vue";
+import CustomButton from "@/components/ui/CustomButton.vue";
+import InputSize from "@/components/ui/InputSize.vue";
 
 export default {
-  name: "TheCalculator",
-  components: {CustomButton},
-  data() {
-    return {
-      numberFormIsActive: 1
-    }
-  },
-  methods: {
+    name: "TheCalculator",
+    components: {CustomButton, InputSize},
+    data() {
+        return {
+            typeNames: [
+                'ПРЯМАЯ',
+                'Г-ОБРАЗНАЯ',
+                'П-ОБРАЗНАЯ',
+            ],
+            numberFormIsActive: 1,
+            inputSizeList1: [
+                { title: 'Сторона А', value: 600 },
+                { title: 'Сторона B', value: 3000 },
+            ],
+            inputSizeList2: [
+                { title: 'Сторона А', value: 1000 },
+                { title: 'Сторона B', value: 3000 },
+                { title: 'Сторона C', value: 600 },
+                { title: 'Сторона D', value: 600 },
+            ],
+            inputSizeList3: [
+                { title: 'Сторона А', value: 1000 },
+                { title: 'Сторона B', value: 3000 },
+                { title: 'Сторона C', value: 2000 },
+                { title: 'Сторона D', value: 1800 },
+                { title: 'Сторона F', value: 600 },
+                { title: 'Сторона E', value: 600 },
+                { title: 'Глубина столешницы', value: 600 },
+            ]
+        }
+    },
+    computed: {
+      getActiveInputSizeList() {
+          const activeArr = 'inputSizeList' + this.numberFormIsActive
+          console.log(activeArr)
+          return this[activeArr]
+      }
+    },
+    methods: {
 
-  }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .wrapper {
-  .calculator {
-    width: 100%;
+    .calculator {
+        width: 100%;
 
-    .header {
-      width: 100%;
-      margin-top: 60px;
+        .header {
+            width: 100%;
+            margin-top: 60px;
 
-      &-title {
-        font-weight: 600;
-        font-size: 30px;
-      }
+            &-title {
+                font-weight: 600;
+                font-size: 30px;
+            }
 
-      &-radio {
-        margin: 40px 0 60px;
+            &-radio {
+                margin: 40px 0 60px;
 
-        .radio-btn {
-          width: calc(100%/3 - 4px/3);
-          border-radius: 0px;
-          text-align: center;
+                .radio-btn {
+                    width: calc(100% / 3 - 4px / 3);
+                    border-radius: 0px;
+                    text-align: center;
+                }
+            }
         }
-      }
+
+        .body {
+            &-img {
+                width: 50%;
+            }
+
+            &-inputs {
+                width: 50%;
+
+                span {
+                    font-size: 20px;
+                    font-weight: 700;
+                }
+
+                .input-list {
+                    width: 100%;
+                    margin-top: 16px;
+                    flex-wrap: wrap;
+
+                    &-item {
+                        flex: 1 1 calc(100%/2);
+                        text-align: center;
+                        margin-bottom: 8px;
+                    }
+                }
+            }
+        }
     }
 
-    .body {
-      &-img {
-        width: 50%;
-      }
-
-      &-text {
-        width: 50%;
-
-        span {
-          font-weight: 700;
-        }
-      }
+    .container {
+        margin: 120px auto 100px;
     }
-  }
 
-  .container {
-    margin: 120px auto 100px;
-  }
-
-  .h-subtitle {
-    margin-bottom: 10px;
-  }
+    .h-subtitle {
+        margin-bottom: 10px;
+    }
 }
 </style>
--- components/ui/InputSize.vue
+++ components/ui/InputSize.vue
@@ -1,50 +1,67 @@
 <template>
-<div class="d-flex align-center flex-column">
+    <div class="d-flex align-center flex-column">
   <span class="title">
     {{ title }}
   </span>
-  <input
-      type="number"
-      :value="value"
-      @input="$emit('input', $event.target.value)"
-      class="input"
-  >
+        <input
+            type="number"
+            :value="value"
+            @input="$emit('input', Number($event.target.value || 0))"
+            class="input"
+        >
 
-</div>
+    </div>
 </template>
 
 <script>
 
 export default {
-  name: "CustomButton",
-  props: {
-    title: {
-      type: String,
-      default: ''
-    },
-    value: {
-      type: Number,
-      required: true
-    },
-  }
+    name: "CustomButton",
+    props: {
+        title: {
+            type: String,
+            default: ''
+        },
+        value: {
+            type: Number,
+            required: true
+        },
+    }
 }
 </script>
 
 <style scoped lang="scss">
 .title {
-  font-weight: 800;
-  align-items: center;
-  font-size: 12px;
-  line-height: 2;
-  text-transform: uppercase;
-  letter-spacing: .05em;
+    font-weight: 400;
+    align-items: center;
+    font-size: 12px;
+    line-height: 2;
+    letter-spacing: .05em;
+    font-size: 18px;
+    margin-bottom: 8px;
 }
+
 .input {
-  border-radius: 100px;
-  &:hover {
+    box-sizing: border-box !important;
+    width: 80px;
+    height: 60px;
+    padding: 10px;
+    text-align: center;
+    background: transparent;
+    border: 1px solid $light;
+    color: $light;
     cursor: pointer;
-  }
-
+    font-size: 20px;
 }
 
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+    -moz-appearance: textfield;
+}
 </style>
